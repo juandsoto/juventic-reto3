8d5a3f0f94997150b2d21f6ffd43a0e6966f5e46
juandsoto <juandsoto1006@gmail.com>
2021-10-22 13:15:30 -0500

    pagina de servicios completada

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,9 +1,49 @@
 import React from 'react';
 
-const Service = () => {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faCircle } from '@fortawesome/free-solid-svg-icons';
+
+const Service = ({ children, id, name, description }) => {
 	return (
 		<>
-			<h1>Service component</h1>
+			<div className="col-lg-4 col-sm-6">
+				<div className="portfolio-item">
+					<a className="portfolio-link" data-bs-toggle="modal" href={`#portafolioModal${id}`}>
+						<div className="portfolio-hover">
+							<div className="portfolio-hover-content"><FontAwesomeIcon icon={faPlus} size='3x'></FontAwesomeIcon></div>
+						</div>
+						{children}
+					</a>
+					<div className="portfolio-caption">
+						<div className="portfolio-caption-heading">{name.toUpperCase()}</div>
+					</div>
+				</div>
+			</div>
+
+
+			<div className="portfolio-modal modal fade" id={`portafolioModal${id}`} tabIndex="-1" role="dialog" aria-hidden="true">
+				<div className="modal-dialog">
+					<div className="modal-content">
+						<div className="close-modal" data-bs-dismiss="modal"><FontAwesomeIcon icon={faCircle} size='2x'></FontAwesomeIcon></div>
+						<div className="container">
+							<div className="row justify-content-center">
+								<div className="col-lg-8">
+									<div className="modal-body">
+										<h2>{name.toUpperCase()}</h2>
+										{children}
+										<p>{description}</p>
+
+										<button className="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
+											<i className="fas fa-times me-1"></i>
+											Cerrar
+										</button>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</>
 	);
 };
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Service from '../components/Service';
+
+import img1 from '../assets/img/portfolio/imagen10.jfif';
+import img2 from '../assets/img/portfolio/imagen12.jfif';
+import img3 from '../assets/img/portfolio/imagen11.jfif';
+import img4 from '../assets/img/portfolio/imagen8.jfif';
+import img5 from '../assets/img/portfolio/imagen6.jfif';
+import img6 from '../assets/img/portfolio/imagen5.jfif';
 
 const Services = () => {
+
+	const [services, setServices] = useState([]);
+
+	const fetchServices = async () => {
+		const res = await fetch('http://localhost:4000/services');
+		const services = await res.json();
+		setServices(services);
+	};
+
+	useEffect(() => {
+		fetchServices();
+	}, []);
+
+	const images = [img1, img2, img3, img4, img5, img6];
+
 	return (
 		<>
-			<h1>Services component</h1>
+			<section className="page-section bg-light" id="portfolio">
+				<div className="container">
+					<div className="text-center">
+						<h2 className="section-heading text-uppercase mt-5">Servicios</h2>
+						<h3 className="section-subheading text-muted">¡Todo para tus celebraciones! </h3>
+					</div>
+					<div className="row">
+						{services.map((service, index) => <Service key={service.id} {...service}>
+							<img className="img-fluid h-50" src={images[index]} alt="..." />
+						</Service>)}
+					</div>
+				</div>
+			</section>
 		</>
 	);
 };

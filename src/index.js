8fe44ad7b02cfcda5fb6e8e298fcf087055168b4
juandsoto <juandsoto1006@gmail.com>
2021-10-22 08:45:25 -0500

    estilos importados

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import './css/normalize.css';
+import './css/skeleton.css';
+import './css/style2.css';
+import './css/styles.css';
 import './index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
